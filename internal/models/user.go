d5cb6fb71ff0c0f6e911f05cc9be80b2257461d6
mikietechie <mzinyoni7@outlook.com>
2024-05-28 22:39:02 +0300

    Wrote my name, all over my code

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,3 +1,13 @@
+/*
+Date Created		1 May 2024
+Author				Mike Z
+Email				mzinyoni7@outlook.com
+Website				https://mikeio.web.app
+Status				Looking for a job!
+Description			A Fintech Data Service
+Inspired by			https://freecurrencyapi.com
+*/
+
 package models
 
 import (
